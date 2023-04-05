ff735b8f1eeeb060d1695af65d75ad4f174d4e12
amittripathir <amittripathir@gmail.com>
2023-04-05 13:00:10 +0530

    this is first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div>
+      <footer className="copyright">
+        <div className="container">
+          <p>
+            © 2023<a href="https://procodebox.com"> Amit Tripathi. </a>All
+            rights reserved.
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
